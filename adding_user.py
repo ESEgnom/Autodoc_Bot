65d927c146111d6b7a6c6bd23816d5470a4e18ca
ESEgnom <gnomese89@gmail.com>
2023-01-15 13:06:52 +0500

    Изменил систему регистрации. добавил рассылку всем пользователям при добавлении запчасти

--- adding_user.py
+++ adding_user.py
@@ -2,24 +2,30 @@ from cfg import *
 
 
 def adding_meneg(message):
-    if message.text.lower() == 'бакалинская' or\
-            message.text.lower() == 'бабушкина' or\
-            message.text.lower() == 'жукова' or\
-            message.text.lower() == 'достоевского' or\
-            message.text.lower() == 'проспект' or\
-            message.text.lower() == 'первомайская':
+    msg = message.text.upper()
+    if msg.startswith('UFU') or\
+            msg.startswith('UFB') or\
+            msg.startswith('UFK') or\
+            msg.startswith('UFD') or\
+            msg.startswith('UFP') or\
+            msg.startswith('UFIM'):
         return True
 
 
 def add_user(message):
-    office_val = (message.text.lower(),)
+    msg = message.text.upper()
+    user_data_list = msg.split(', ')
+    print(f'user_data_list {user_data_list} {type(user_data_list)}')
+    user_data = user_data_list[0]
+    print(f'user_data {user_data} {type(user_data)}')
     sql_select_query = 'SELECT office_id from office WHERE office_address = ?'
-    office_id = cur.execute(sql_select_query, (office_val)).fetchone()
+    office_id = cur.execute(sql_select_query, (user_data,)).fetchone()
+    print(f'office_id {office_id}')
     try:
         cur.execute('INSERT INTO user(chat_id, user_name, office_id) VALUES (?, ?, ?)',
-                    (message.chat.id, message.chat.first_name, office_id[0]))
+                    (message.chat.id, user_data_list[1], office_id[0]))
         bot.send_message(message.chat.id, 'Теперь ты есть в моей базе и можешь добавлять свои запчасти или '
                                           'искать запчасти других менеджеров. Введи команду /help что-бы увидеть список команд.')
         connection.commit()
     except sqlite3.IntegrityError as error:
-        bot.send_message(message.chat.id, f'Так вышло, что ты уже есть в базе {error}')
+        bot.send_message(message.chat.id, f'Так вышло, что ты уже есть в базе --> {error}')
---
+++ mailer.py
@@ -0,0 +1,28 @@
+from cfg import *
+
+
+def create_distribution_list():
+    sql_select_query = 'SELECT chat_id from user'
+    billet = cur.execute(sql_select_query).fetchall()
+    print(type(billet))
+    mail_list = []
+
+    for item in billet:
+        for jtem in item:
+            mail_list.append(jtem)
+
+    print(f'create_mail_list {mail_list}')
+    return mail_list
+
+def send_newsletter(message, part_info):
+    distribution_list = create_distribution_list()
+    sql_select_query = 'SELECT user_name FROM user where chat_id= ?'
+    user_name = cur.execute(sql_select_query, (message.chat.id,)).fetchone()
+    print(f'billet {user_name} {type(user_name)}')
+
+    for chat_id in distribution_list:
+        if message.chat.id != chat_id:
+            print(f'Сообщение для {chat_id}')
+            print(f'{user_name[0]} добавил {part_info}')
+            bot.send_message(chat_id, f'{user_name[0]} добавил {part_info}')
+
